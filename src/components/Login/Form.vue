8cd0cb0464f9a837bcc40e4ac95eebca2650b9ef
Göktürk DOĞAN <gokturk.dogan@hotmail.com>
2025-06-08 16:05:53 +0300

    login screen UI created

---
+++ src/components/Login/Form.vue
@@ -0,0 +1,147 @@
+<template>
+  <div class="form">
+    <div class="form__header">
+        <h2 class="form__title">Lütfen Giriş Yapınız</h2>
+        <h4 class="form__subTitle">Lütfen bilgilerinizi giriniz.</h4>
+    </div>
+    <div class="form__body">
+        <div class="form__group">
+            <label class="form__inputTitle">Email*</label>
+            <input type="text" class="form__input" placeholder="example@example.com">
+        </div>
+        <div class="form__group">
+            <label class="form__inputTitle">Şifre*</label>
+            <input type="password" class="form__input" placeholder="*******">
+        </div>
+        <div class="form__actions">
+            <div class="form__action">            
+                <input type="checkbox" class="form__checkBox">
+                <label class="form__inputTitle">Beni hatırla</label>
+            </div>
+            <div class="form__action">
+                <label class="form__inputTitle">Şifremi unuttum</label>      
+                <a href="/" class="form__link">Sıfırla</a>
+            </div>
+        </div>
+    </div>
+    <div class="form__footer">
+        <button class="form__btn">Giriş Yap</button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "login-form",
+  data() {},
+  components: {},
+  created() {},
+  methods: {},
+};
+</script>
+<style lang="scss" scoped>
+    .form {
+        background-color: #0F1021;
+        border-radius: 20px;
+        padding: 40px 40px 50px;
+        border: 1px solid #21234B;
+        width: 520px;
+
+        &__header {
+            color: #CCCEEF;
+            text-align: center;
+        }
+
+        &__title {
+            font-size: 30px;
+            line-height: 40px;
+            margin-bottom: 15px;
+        }
+
+        &__subTitle {
+            font-size: 16px;
+            line-height: 24px;
+            margin-bottom: 33px;
+        }
+
+        &__body {
+
+        }
+
+        &__group {
+            display: flex;
+            flex-direction: column;
+            margin-bottom: 24px;
+        }
+        
+        &__inputTitle {
+            color: #D4D5D1;
+            font-size: 16px;
+        }
+
+        &__input {
+            margin-top: 6px;
+            padding: 0 24px;
+            height: 50px;
+            border-radius: 6px;
+            font-size: 16px;
+            transition: 0.3s ease;
+            color: #CCCEEF;
+            background-color: #0F1021;
+            border: 1px solid #21234B;
+            caret-color: #CCCEEF;
+            outline: none;
+
+            &:focus {
+                border-color: #7130C3;
+            }
+        }
+
+        &__actions {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+        &__action {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+        &__checkBox {
+            margin-right: 10px;
+            cursor: pointer;
+            border-radius: .25em;
+        }
+
+        &__link {
+            margin-left: 10px;
+        }
+
+        &__footer {
+            margin-top: 30px;
+        }
+
+        &__btn {
+            cursor: pointer;
+            width: 100%;
+            border-radius: 6px;
+            -webkit-box-pack: center;
+            justify-content: center;
+            font-size: 18px;
+            font-weight: 600;
+            padding: 15px 30px;
+            color: #FFFFFF;
+            border-color: #7130C3;
+            background-color: #7130C3;
+            transition: 0.3s ease;
+
+            &:hover {
+                background-color: #FF3BD4;
+                border-color: #FF3BD4;
+            }
+        }
+    }
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  server: {
+    host: '0.0.0.0'
+  },
+  base: '/coin-actions/',
+  css: {
+    preprocessorOptions: {
+/*       scss: {
+        additionalData: `
+          @use './src/assets/scss/variables/_colors' as *;
+          @use './src/assets/scss/mixins/_mediaQueries' as *;
+        `,
+      }, */
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+});

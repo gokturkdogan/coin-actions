dacae1c0d595945abc521f12e75920774914c53c
Göktürk DOĞAN <gokturk.dogan@hotmail.com>
2025-06-09 11:13:41 +0300

    forgot password feature

---
+++ src/layout/Forgot.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Forgot />
+  </div>
+</template>
+
+<script>
+import Forgot from '../views/Forgot.vue'
+export default {
+  name: "forgot-layout",
+  data() {
+    return {}
+  },
+  components: {
+    Forgot
+  },
+  created() {},
+  methods: {},
+};
+</script>

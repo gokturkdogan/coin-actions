64c22c8b759f87eb2fd219d9420d1b436e9f446d
Göktürk DOĞAN <gokturk.dogan@hotmail.com>
2025-07-16 15:45:51 +0300

    gh pages prepare

--- src/config/api.js
+++
@@ -1,5 +0,0 @@
-const api = {
-    coinsWith24Hr: 'api/v3/ticker/24hr'
-};
-
-export default api;
--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store/index.js';
-import router from './router'
-import './assets/scss/main.scss'
-
-createApp(App).use(store).use(router).mount('#app')
